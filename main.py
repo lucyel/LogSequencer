0696922f02c363db77b4f311b15b1073470696c1
Lucy <32479481+lucyel@users.noreply.github.com>
2022-03-16 09:14:00 +0700

    Update main.py
    update webhook

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from yaml.loader import SafeLoader
 from elasticsearch import Elasticsearch
 from flask import Flask, request, abort
 
-
-# TODO: web interface for tools
 # TODO: Make function to convert anytime format to usable time format.
 
 urllib3.disable_warnings()
@@ -27,25 +25,23 @@ def check_indices(indices):
     return es.indices.exists(index=indices)
 
 
-def create_index(indices, request_body):
-    es.indices.create(index=indices, body=request_body)
-
-
-# TODO: add more search type
-# TODO: web interface need a list of type to use
-
-
 def search_query(index, key, value, value_must_exsists, gte, lte, querytype='match', size=10):
-    if querytype == "match":
-        body = {"bool": {"must": [{"match": {key: value}}, {"range": {"@timestamp": {"gte": gte, "lte": lte, "format":"strict_date_optional_time||epoch_millis"}}},{"exists":{"field":value_must_exsists}}]}}
+    if check_indices(index):
+        if querytype == "match":
+            body = {"bool": {"must": [{"match": {key: value}}, {"range": {"@timestamp": {"gte": gte, "lte": lte, "format":"strict_date_optional_time||epoch_millis"}}},{"exists":{"field":value_must_exsists}}]}}
+            return es.search(index=index, query=body, size=size)
+    else:
+        print("index not found")
+        return exit(1)
 
-    return es.search(index=index, query=body, size=size)
 
-
-def search_query_string(index, value, size,field_must_exsists):
-    # body = {"query_string":{"query":value},"bool": {"must": [{"exists":{"field":field_must_exsists}}]}}
-    body = {"bool": {"must": [{"query_string": {"query": value}}, {"exists": {"field": field_must_exsists}}]}}
-    return es.search(index=index, query=body, size=size)
+def search_query_string(index, value, field_must_exsists, gte, lte,  size):
+    if check_indices(index):
+        body = {"bool":{"must":[{"query_string":{"query":value}},{"exists":{"field":field_must_exsists}},{"range":{"@timestamp":{"gte":gte,"lte":lte,"format":"strict_date_optional_time||epoch_millis"}}}]}}
+        return es.search(index=index, query=body, size=size)
+    else:
+        print("index not found")
+        return exit(1)
 
 
 def split_value(data, delimiter):
@@ -63,79 +59,42 @@ def count_chain(arg):
     return True
 
 def convert_args_to_json(args):
-    return "body"
+    body = []
 
+    body0 = {
+        "inputKeyword": "user.name",
+        "inputValue": "DESKTOP-4R0S3HP\mio",
+        "inputIndices": "auditbeat-*",
+        "outputKeyword": "process.name",
+        "gte": "now-1y",
+        "lte": "now"
+    }
 
-es = init_connection(f"{vari['elastic_authen']['host']}", f"{vari['elastic_authen']['scheme']}",
-                     f"{vari['elastic_authen']['idkey']}", f"{vari['elastic_authen']['apikey']}", True, False)
+    body0_copy = body0.copy()
+    body.append(body0_copy)
 
+    body1 = {
+        "inputIndices": "winlogbeat-*",
+        "outputKeyword": "event.code",
+        "gte": "now-1y",
+        "lte": "now"
+    }
 
+    body1_copy = body1.copy()
+    body.append(body1_copy)
 
-#
-# for i in range(len(count_chain("args"))):
-#     body_temp = convert_args_to_json("args")
-#     body_temp_copy = body_temp.copy()
-#     body.append(body_temp_copy)
+    body2 = {
+        "inputIndices": "test-*",
+        "outputKeyword": "winlog.event_data.QueryName",
+        "gte": "now-1y",
+        "lte": "now"
+    }
 
-# body = []
-# body0 = {
-#     "inputKeyword": "user.name",
-#     "inputValue": "DESKTOP-4R0S3HP\mio",
-#     "inputIndices": "auditbeat-*",
-#     "outputKeyword": "process.name",
-#     "gte": "now-1y",
-#     "lte": "now"
-# }
-#
-# body0_copy = body0.copy()
-# body.append(body0_copy)
-#
-# body1 = {
-#     "inputIndices": "winlogbeat-*",
-#     "outputKeyword": "event.code",
-#     "gte": "now-1y",
-#     "lte": "now"
-# }
-#
-# body1_copy = body1.copy()
-# body.append(body1_copy)
-#
-#
-# body2 = {
-#     "inputIndices": "winlogbeat-*",
-#     "outputKeyword": "winlog.event_data.QueryName",
-#     "gte": "now-1y",
-#     "lte": "now"
-# }
-#
-# body2_copy = body2.copy()
-# body.append(body2_copy)
-#
-# body3 = {
-#     "inputIndices": "winlogbeat-*",
-#     "outputKeyword": "winlog.event_data.ProcessId",
-#     "gte": "now-1y",
-#     "lte": "now"
-# }
-#
-# body3_copy = body3.copy()
-# body.append(body3_copy)
-#
-# body4 = {
-#     "inputIndices": "winlogbeat-*",
-#     "outputKeyword": "winlog.event_data.Image",
-#     "gte": "now-1y",
-#     "lte": "now"
-# }
-#
-# body4_copy = body4.copy()
-# body.append(body4_copy)
-#
-# print(body)
+    body2_copy = body2.copy()
+    body.append(body2_copy)
+    return body
 
-
-#body format [{}, {}, {}]
-
+#input body data must be a list
 def search(body):
     result_list = []
     output_res = []
@@ -152,6 +111,7 @@ def search(body):
             for value in outKey_temp:
                 count_field += 1
 
+            # Get each field name in the json format and output data to temp value
             if count_field == 1:
                 if body[0]["outputKeyword"] == "*":
                     outputValue = input_result['hits']['hits'][0]['_source']
@@ -169,6 +129,11 @@ def search(body):
                     outputValue = input_result['hits']['hits'][i]['_source'][outKey_temp[0]][outKey_temp[1]][outKey_temp[2]]
                     if outputValue not in output_res_temp:
                         output_res_temp.append(outputValue)
+            elif count_field == 4:
+                for i in range(len(input_result['hits']['hits'])):
+                    outputValue = input_result['hits']['hits'][i]['_source'][outKey_temp[0]][outKey_temp[1]][outKey_temp[2]][outKey_temp[3]]
+                    if outputValue not in output_res_temp:
+                        output_res_temp.append(outputValue)
             count_field = 0
             output_res.append(output_res_temp)
             output_res_temp = []
@@ -177,7 +142,9 @@ def search(body):
             for value in outKey_temp:
                 count_field += 1
             for i in range(len(output_res[args_value-1])):
-                input_result = search_query_string(body[args_value]['inputIndices'], output_res[args_value-1][i], 1000, body[args_value]['outputKeyword'])
+                input_result = search_query_string(body[args_value]['inputIndices'], output_res[args_value-1][i], body[args_value]['outputKeyword'], body[args_value]['gte'], body[args_value]['lte'], 1000)
+
+                # Get each field name in the json format and output data to temp value
                 if count_field == 1:
                     if body[args_value]["outputKeyword"] == "*":
                         outputValue = input_result['hits']['hits'][0]['_source']
@@ -195,6 +162,11 @@ def search(body):
                         outputValue = input_result['hits']['hits'][i]['_source'][outKey_temp[0]][outKey_temp[1]][outKey_temp[2]]
                         if outputValue not in output_res_temp:
                             output_res_temp.append(outputValue)
+                elif count_field == 4:
+                    for i in range(len(input_result['hits']['hits'])):
+                        outputValue = input_result['hits']['hits'][i]['_source'][outKey_temp[0]][outKey_temp[1]][outKey_temp[2]][outKey_temp[3]]
+                        if outputValue not in output_res_temp:
+                            output_res_temp.append(outputValue)
             output_res.append(output_res_temp)
             output_res_temp = []
             count_field = 0
@@ -203,19 +175,24 @@ def search(body):
     for i in range(len(output_res)):
         if output_res[i]:
             print(output_res[i])
+    return output_res
 
 
 app = Flask(__name__)
 @app.route('/request', methods=['POST', 'GET'])
 def get_webhook():
     if request.method == 'POST':
-        body = [request.json]
-        search(body)
-        return 'success', 200
+        body = request.json
+        result = search(body['body'])
+        return str(result), 200
     else:
         abort(400)
 
 
 if __name__ == '__main__':
+    es = init_connection(f"{vari['elastic_authen']['host']}", f"{vari['elastic_authen']['scheme']}",
+                         f"{vari['elastic_authen']['idkey']}", f"{vari['elastic_authen']['apikey']}", True, False)
     app.run()
+    # body = convert_args_to_json("argc")
+    # print(body)
     # search(body)
